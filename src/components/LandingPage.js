2d85c56f5cf99b59ec8d64fe91b97b0f451651ce
Janita M <jaymadr@gmail.com>
2020-04-26 15:07:19 -0400

    Add a hook to navigate to the home page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { not } from 'ramda';
 import './LandingPage.css';
+import HomePage from './HomePage';
 
 const LandingPage = () => {
+  const [isLandingPage, setIsLandingPage] = useState(true);
+
+  if (not(isLandingPage)) {
+    return (
+      <main>
+        <HomePage
+          setIsLandingPage={setIsLandingPage}
+        />
+      </main>
+    );
+  }
+
   return (
     <div className="LandingPage">
-      <div className="LandingPage-showcase-content">
-        <h2 className="LandingPage-heading">Tides API</h2>
-        <p className="LandingPage-lead">
-          Know when the flood is coming...
-        </p>
-        <a href="/home" className="LandingPage-btn">Read More</a>
+      <div className="LandingPage-showcase">
+        <h2>Tides API</h2>
+        <p>Know when the flood is coming...</p>
+        <button onClick={() => setIsLandingPage(false)}>Read More</button>
       </div>
-    </div>
+    </div >
   );
-
 }
 
 export default LandingPage;
